1e9182f97c597dcdf34d22da6cbbb7ffc78dce41
Yuanzheng Li <yuanzhengliwork@gmail.com>
2018-08-09 23:07:38 -0700

    add transition styles to hands and make hour hand and minute hand shorter

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -8,14 +8,17 @@ function setTime() {
   const second = date.getSeconds();
   const secondHandDeg = (second / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondHandDeg}Deg)`;
+  secondHand.style.transition = secondHandDeg === 90 ? 'all 0s' : 'all 0.05s';
 
   const minute = date.getMinutes();
   const minHandDeg = (minute / 60) * 360 + (second / 60) * (360 / 60) + 90;
   minHand.style.transform = `rotate(${minHandDeg}Deg)`;
+  minHand.style.transition = minHandDeg=== 90 ? 'all 0s' : 'all 0.05s';
 
   const hour = date.getHours();
   const hourHandDeg = (hour / 12) * 360 + (minute / 60) * (360 / 12) + 90;
   hourHand.style.transform = `rotate(${hourHandDeg}Deg)`;
+  hourHand.style.transition = hourHandDeg === 90 ? 'all 0s' : 'all 0.05s';
 }
 
 setTime();
