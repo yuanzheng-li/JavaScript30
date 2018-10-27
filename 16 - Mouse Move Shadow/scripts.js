87d6af299cc2838dc91a50e2a9c374133007986c
Yuanzheng Li <yuanzhengliwork@gmail.com>
2018-10-27 12:47:33 -0700

    Day 16 - Mouse Move Shadow

---
+++ 16 - Mouse Move Shadow/scripts.js
@@ -0,0 +1,22 @@
+const hero = document.querySelector(".hero");
+const text = document.querySelector("h1");
+const walk = 100;
+
+hero.addEventListener("mousemove", shadow);
+
+function shadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+
+  if (this !== e.target) {
+    x += e.target.offsetLeft;
+    y += e.target.offsetTop;
+  }
+
+  const xWalk = Math.round((x / width) * walk - walk / 2);
+  const yWalk = Math.round((y / height) * walk - walk / 2);
+
+  text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7)
+  `;
+}
