b1ff0d7c49350c3046939f2977a1a70ca0eb82e4
Yuanzheng Li <yuanzhengliwork@gmail.com>
2018-10-30 19:57:55 -0700

    Add Day 18

---
+++ 18 - Adding Up Times with Reduce/scripts.js
@@ -0,0 +1,19 @@
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map((time) => {
+    const [mins, secs] = time.split(':').map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((totalSeconds, second) => totalSeconds + second);
+
+let secondsLeft = seconds;
+
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft %= 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft %= 60;
+
+console.log(hours, mins, secondsLeft);
